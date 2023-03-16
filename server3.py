e58a77e630ebe8c75e3de59cde11632f6a64411d
Kunal <76587209+Pikel1997@users.noreply.github.com>
2023-03-16 12:09:15 +0530

    Optimised
    I won't elaborate further

--- server3.py
+++ server3.py
@@ -1,52 +1,40 @@
-# server3.py on EC2 instance
 import socket
-from threading import Thread
-from SocketServer import ThreadingMixIn
+from concurrent.futures import ThreadPoolExecutor
 
-# TCP_IP = socket.gethostbyaddr("your-ec2-public_ip")[0]
-TCP_IP = socket.gethostbyaddr("3.129.88.101")[0] #(Example)
+TCP_IP = socket.gethostbyaddr("3.129.88.101")[0]
 TCP_PORT = 9001
 BUFFER_SIZE = 1024
+FILE_NAME = 'mytext.txt'
 
-print 'TCP_IP=',TCP_IP
-print 'TCP_PORT=',TCP_PORT
+print('TCP_IP=',TCP_IP)
+print('TCP_PORT=',TCP_PORT)
 
-class ClientThread(Thread):
-
-    def __init__(self,ip,port,sock):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.sock = sock
-        print " New thread started for "+ip+":"+str(port)
-
-    def run(self):
-        filename='mytext.txt'
-        f = open(filename,'rb')
+def send_file(conn):
+    with open(FILE_NAME, 'rb') as f:
         while True:
             l = f.read(BUFFER_SIZE)
-            while (l):
-                self.sock.send(l)
-                #print('Sent ',repr(l))
-                l = f.read(BUFFER_SIZE)
             if not l:
-                f.close()
-                self.sock.close()
                 break
+            conn.sendall(l)
+    conn.close()
 
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind((TCP_IP, TCP_PORT))
-threads = []
+def handle_client(conn, addr):
+    print('Got connection from', addr)
+    try:
+        send_file(conn)
+    except Exception as e:
+        print('Error:', e)
 
-while True:
-    tcpsock.listen(5)
-    print "Waiting for incoming connections..."
-    (conn, (ip,port)) = tcpsock.accept()
-    print 'Got connection from ', (ip,port)
-    newthread = ClientThread(ip,port,conn)
-    newthread.start()
-    threads.append(newthread)
+def main():
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpsock:
+            tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            tcpsock.bind((TCP_IP, TCP_PORT))
+            tcpsock.listen(5)
+            print('Waiting for incoming connections...')
+            while True:
+                conn, addr = tcpsock.accept()
+                executor.submit(handle_client, conn, addr)
 
-for t in threads:
-    t.join()
+if __name__ == '__main__':
+    main()
